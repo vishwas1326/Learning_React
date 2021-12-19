8caba6640139600d863ada98254365039502877b
Sai Vishwas Bejawada <Saivishwas.B@WinWire.com>
2021-12-19 00:20:27 -0600

    https://github.com/vishwas1326/PracticeReact.git

---
+++ amazon/build/precache-manifest.9f0acca0fed5f11ebd7ab5cd8153029d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78030b9d91a6c80193f0d9dfe479abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "736ebcaa224a28702b4e",
+    "url": "/static/css/main.51f97373.chunk.css"
+  },
+  {
+    "revision": "abd6bb93da4b568d1abb",
+    "url": "/static/js/2.3807e095.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.3807e095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736ebcaa224a28702b4e",
+    "url": "/static/js/main.2759dc93.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
